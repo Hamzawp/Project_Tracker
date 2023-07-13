2b0efab9f56682a2ac96a2fdb5047517c11c379a
Hamza Sayyed <hamzawp404@gmail.com>
2023-07-13 22:43:12 +0530

    project completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import Clients from "./components/Clients";
-import Header from "./components/Header";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import AddClientModal from "./components/AddClientModal";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -22,9 +24,8 @@ const cache = new InMemoryCache({
   },
 });
 
-
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: 'http://localhost:8000/graphql',
   cache,
 });
 
@@ -32,11 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );

4a11a55cdf20c59811d0957b46204bbf3281b0de
Hamza Sayyed <hamzawp404@gmail.com>
2023-07-13 23:08:25 +0530

    deployment ready

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./server/schema/schema');
 const connectDB = require('./server/config/db');
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 const app = express()
 connectDB()
 app.use(cors())
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/graphql',
     graphqlHTTP({
@@ -17,5 +19,8 @@ app.use('/graphql',
         graphiql: process.env.NODE_ENV === 'development'
     })
 )
-
+app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname, './client/build', 'index.html');
+    res.sendFile(indexPath);
+});
 app.listen(port, console.log(`Server running on port ${port}`))
